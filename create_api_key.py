8728e251e13dfb4f1527a8c3fb9a20ea8216767a
xu742822593-droid <xu742822593@gmail.com>
2026-07-20 18:42:46 +0800

    Update create_api_key.py

--- create_api_key.py
+++ create_api_key.py
@@ -1,6 +1,7 @@
+import os
 from py_clob_client.client import ClobClient
 
-PRIVATE_KEY = 0xcff40796843e9d54dc0942d361cada6701bce32332cb692fb727c5d9e9d834bf
+PRIVATE_KEY = os.getenv("POLY_PRIVATE_KEY")
 
 client = ClobClient(
     host="https://clob.polymarket.com",
@@ -12,5 +13,3 @@ creds = client.create_or_derive_api_creds()
 print("POLY_API_KEY=" + creds.api_key)
 print("POLY_API_SECRET=" + creds.api_secret)
 print("POLY_API_PASSPHRASE=" + creds.api_passphrase)
-print("POLY_PRIVATE_KEY=" + PRIVATE_KEY)
-
